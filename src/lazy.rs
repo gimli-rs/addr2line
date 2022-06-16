7e4a284103a27a0030dd140e1e0f88a23804326f
peamaeq <104552320+peamaeq@users.noreply.github.com>
2022-06-16 16:40:07 +0800

    Reduce scope of unsafe block (#247)

--- src/lazy.rs
+++ src/lazy.rs
@@ -11,19 +11,17 @@ impl<T> LazyCell<T> {
     }
 
     pub fn borrow_with(&self, closure: impl FnOnce() -> T) -> &T {
-        unsafe {
-            // First check if we're already initialized...
-            let ptr = self.contents.get();
-            if let Some(val) = &*ptr {
-                return val;
-            }
-            // Note that while we're executing `closure` our `borrow_with` may
-            // be called recursively. This means we need to check again after
-            // the closure has executed. For that we use the `get_or_insert`
-            // method which will only perform mutation if we aren't already
-            // `Some`.
-            let val = closure();
-            (*ptr).get_or_insert(val)
+        // First check if we're already initialized...
+        let ptr = self.contents.get();
+        if let Some(val) = unsafe { &*ptr } {
+            return val;
         }
+        // Note that while we're executing `closure` our `borrow_with` may
+        // be called recursively. This means we need to check again after
+        // the closure has executed. For that we use the `get_or_insert`
+        // method which will only perform mutation if we aren't already
+        // `Some`.
+        let val = closure();
+        unsafe { (*ptr).get_or_insert(val) }
     }
 }
