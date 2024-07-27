82f34e5ef98d3fa0948b5890d41bff5bbe08812b
Martin Liska <martin.liska@hey.com>
2024-07-27 11:32:01 +0200

    Add Docker wrapper script

---
+++ benchmark-addr2line-docker.sh
@@ -0,0 +1,4 @@
+IMAGE_NAME=opensuse-addr2line
+
+docker build -t $IMAGE_NAME -f Dockerfile-opensuse .
+docker run -v `pwd`:/addr2line -it $IMAGE_NAME bash -c "cd /addr2line && cargo build --release --features bin && ./benchmark-addr2line.sh"
