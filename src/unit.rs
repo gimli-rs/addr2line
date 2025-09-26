2be7d3ee4c36ab7bec06e8144f505fdb591212dd
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2025-09-26 20:08:05 +0200

    Sort parsed unit ranges by `unit_id` as well
    Not necessary for correctness, but helps compare results.

--- src/unit.rs
+++ src/unit.rs
@@ -362,7 +362,7 @@ impl<R: gimli::Reader> ResUnits<R> {
         }
 
         // Sort this for faster lookup in `Self::find_range`.
-        unit_ranges.sort_unstable_by_key(|i| i.range.end);
+        unit_ranges.sort_unstable_by_key(|i| (i.range.end, i.unit_id));
 
         // Calculate the `min_begin` field now that we've determined the order of
         // CUs.
